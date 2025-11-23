1c2760988f6cc55392863d932954985ba1a02391
David <65056778+dpami507@users.noreply.github.com>
2025-11-23 01:13:26 -0500

    ok so now I think there are more stashes
    its 1am why is this my life this was suposed to be easy

--- core/shaderz/Skybox.cpp
+++ core/shaderz/Skybox.cpp
@@ -86,39 +86,97 @@ namespace shaderz {
 
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		{
+			std::cout << "Converting face " << i << std::endl;
+
+			conversionShader->setMat4("view", captureViews[i]);
+
+
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, envCubemap, 0);
+			
+			GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+			if (status != GL_FRAMEBUFFER_COMPLETE)
+			{
+				std::cout << "Framebuffer not complete for face " << i << " - status: 0x" << std::hex << status << std::dec << "\n";
+			}
+
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			CheckError("SIDE");
+			renderCube();
+			//glBindVertexArray(skyboxVAO);
+			//glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		glBindTexture(GL_TEXTURE_CUBE_MAP, envCubemap);
+		glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+
+		CheckError("CUBEMAP");
+	}
+	//Create irradiance map
+	void Skybox::createIrradianceMap()
+	{
+		std::cout << "Creating Irradiance Map\n";
+
+		glGenTextures(1, &irradianceMap);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, irradianceMap);
+		for (unsigned int i = 0; i < 6; ++i)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 32, 32, 0, GL_RGB, GL_FLOAT, nullptr);
+		}
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-		glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
-	}
-
-	void Skybox::loadCubemap()
-	{
-		glGenTextures(1, &cubemapTexture);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
-
-		for (unsigned int i = 0; i < faces.size(); i++)
-		{
-			stbi_set_flip_vertically_on_load(false);
-			unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
-			if (data)
-			{
-				int format = (nrChannels == 4) ? GL_RGBA : (nrChannels == 3) ? GL_RGB : (nrChannels == 2) ? GL_RG : GL_RED;
-
-				std::cout << "Cubemap loaded path: " << faces[i] << " (channels: " << nrChannels << ")" << std::endl;
-				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-				stbi_image_free(data);
-			}
-			else
-			{
-				std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
-				stbi_image_free(data);
-			}
-		}
-
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
+		glBindRenderbuffer(GL_RENDERBUFFER, captureRBO);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, 32, 32);
+
+		irradianceShader->use();
+		irradianceShader->setInt("environmentMap", 0);
+		irradianceShader->setMat4("projection", captureProjection);
+
+		//Idk which one to use
+		glActiveTexture(GL_TEXTURE0);
+		//glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, envCubemap);
+
+		glViewport(0, 0, 32, 32);
+		glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
+		//glBindVertexArray(skyboxVAO);
+
+		for (unsigned int i = 0; i < 6; ++i)
+		{
+			std::cout << "Creating irradianceCubemap face " << i << std::endl;
+
+			irradianceShader->setMat4("view", captureViews[i]);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, irradianceMap, 0);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			renderCube();
+			//glBindVertexArray(skyboxVAO);
+			//glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		CheckError("IRRADIANCE");
+	}
+	//Create the prefilter map
+	void Skybox::createPrefilterMap()
+	{
+		std::cout << "Creating Prefilter Cubemap\n";
+
+		glGenTextures(1, &prefilterMap);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, prefilterMap);
+		for (unsigned int i = 0; i < 6; ++i)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 128, 128, 0, GL_RGB, GL_FLOAT, nullptr);
+		}
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
